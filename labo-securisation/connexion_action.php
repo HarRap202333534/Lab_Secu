3dbac25307041f99e252f584d0cd30ade0ea50f9
2333534 <raphaelharvey15@gmail.com>
2024-09-18 16:51:16 -0400

    Added final version

--- labo-securisation/connexion_action.php
+++ labo-securisation/connexion_action.php
@@ -1,10 +1,62 @@
 <?php
 session_start();
 
+function addAttempt($user, $con){
+    $sql = "UPDATE user_attempt SET attempts = ((SELECT attempts FROM user_attempt WHERE user_id = ?) + 1) WHERE user_id = ?";
+    $result = $con->prepare($sql);
+    $result->execute([$user, $user]);
+    $result->closeCursor();
+    if(getAttempt($user, $con)[0] == 5){
+        $sql = "UPDATE user_attempt SET blocked = 1, last_attempt = NOW() WHERE user_id = ?";
+        $result = $con->prepare($sql);
+        $result->execute([$user]);
+        $result->closeCursor();
+    }
+}
+
+function getAttempt($user, $con){
+    $sql = "SELECT attempts FROM user_attempt WHERE user_id = ?";
+    $result = $con->prepare($sql);
+
+    $result->execute([$user]);
+
+    $attempts = $result->fetch();
+
+    return $attempts;
+}
+function getTempsBloqueRestant($user, $con){
+    $sql = "SELECT TIMESTAMPDIFF(MINUTE, NOW(), (ADDTIME(last_attempt, '0:15:0'))) FROM user_attempt WHERE user_id = ?";
+    $result = $con->prepare($sql);
+
+    $result->execute([$user]);
+
+    $temps = $result->fetch();
+
+    return $temps;
+}
+function resetAttempts($user, $con) {
+    $sql = "UPDATE user_attempt SET blocked = 0, attempts = 0 WHERE user_id = ?";
+    $result = $con->prepare($sql);
+
+    $result->execute([$user]);
+    $result->closeCursor();
+}
+function getBlockedEtat($user, $con){
+    $sql = "SELECT blocked FROM user_attempt WHERE user_id = ?";
+    $result = $con->prepare($sql);
+
+    $result->execute([$user]);
+
+    $blocked = $result->fetch();
+
+    return $blocked;
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $servername = "localhost";
     $username = "root"; 
-    $password = "cegep";
+    $password = "";
     $dbname = "mydatabase";
 
     try {
@@ -14,23 +66,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $inputUsername = $_POST['username'];
         $inputPassword = $_POST['password'];
 
-        $sql = "SELECT * FROM utilisateurs WHERE username = '$inputUsername'";
-        $result = $conn->query($sql);
+        $sql = "SELECT * FROM users WHERE username = ?";
+        $result = $conn->prepare($sql);
+
+        $result->execute([$inputUsername]);
+
         $user = $result->fetch(PDO::FETCH_ASSOC);
 
-        
-        if ($inputPassword == $user['password'])
-        {
-            $_SESSION['username'] = $user['username'];
-            header("Location: acceuil.php");
-            exit();
-        } else {
-            $error = "Nom d'utilisateur ou mot de passe incorrect.";
+        if(getBlockedEtat($user['user_id'], $conn)[0] == 1){
+            if(getTempsBloqueRestant($user['user_id'], $conn)[0] <= 0){
+                resetAttempts($user['user_id'], $conn);
+            }
         }
+        if(getAttempt($user['user_id'], $conn)[0] < 5) {
+            if ($inputPassword == $user['password'])
+            {
+                resetAttempts($user['user_id'], $conn);
+                $_SESSION['username'] = $user['username'];
+                header("Location: acceuil.php");
+                exit();
+            } else {
+                addAttempt($user['user_id'], $conn);
+                $error = "Mauvais mot de passe ou nom d'utilisateur.";
+            }
+        }
+        else{
+            $error = "Trop de tentatives de connexion, compte bloqué pendant " . getTempsBloqueRestant($user['user_id'], $conn)[0] . " minutes.";
+        }
+        header("Location: connexion.php?erreur=".$error."");
     } catch(PDOException $e) {
         echo "Erreur: " . $e->getMessage();
     }
-
     $conn = null;
 }
 ?>
---
+++ labo-securisation/pdo.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+    $username = "root"; 
+    $password = "";
+    $dbname = "mydatabase";
+
+try{
+    $con = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    die("Connexion échouée. Erreur : " . $e->getMessage());
+    
+}
+
+?>
